91cc11ff783f976bfae4ca3e7a7b41c6aee1abd1
中山赢友网络科技有限公司 <winu@winu.net>
2016-02-11 22:16:49 +0800

    1、编写默认实例

---
+++ App.Site/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using App.Entity;
+using App.IServices;
+
+namespace App.Site.Controllers
+{
+    // 继承BaseController
+    public class DefaultController : BaseController
+    {
+        // 构造方法注入
+        public DefaultController(IUserServices _IUserServices)
+        {
+            base._IUserServices = _IUserServices;
+        }
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
