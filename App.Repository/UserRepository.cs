7465e10c8eceb2244817633ce4f63bc7c3e006e1
中山赢友网络科技有限公司 <winu@winu.net>
2016-02-11 22:21:35 +0800

    1、格式化T4模板生成器核心代码，更加易读 2、新增使用示例 3、更新文档

--- App.Repository/UserRepository.cs
+++ App.Repository/UserRepository.cs
@@ -5,7 +5,7 @@ using App.IRepository;
 /*!
 * 文件名称：UserRepository仓储类
 * 文件作者：新生帝
-* 编写日期：2016-02-11 22:15:47
+* 编写日期：2016-02-11 22:20:12
 * 版权所有：中山赢友网络科技有限公司
 * 企业官网：http://www.winu.net
 * 开源协议：GPL v2 License
