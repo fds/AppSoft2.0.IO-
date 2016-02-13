11ae52a92128f9e10dd397a8daf7ca632f4e9c78
中山赢友网络科技有限公司 <winu@winu.net>
2016-02-14 00:15:52 +0800

    1、添加发布程序集

--- App.RESTful API/Global.asax.cs
+++ App.RESTful API/Global.asax.cs
@@ -11,9 +11,11 @@ namespace App.RESTful_API
     {
         protected void Application_Start()
         {
+            // 依赖注入所有程序集
             Bootstrapper.StartRegisterDependencies();
 
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            // 设置默认将所有对象自动输出为Json格式
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
     }
