6feb44f69a4a6fc805ce44fc3b12d1dd06960fe8
中山赢友网络科技有限公司 <winu@winu.net>
2016-02-14 21:56:53 +0800

    1、小更新

--- App.ORM/SqlSugarTablesMapping.cs
+++ App.ORM/SqlSugarTablesMapping.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 /*!
 * 文件名称：SqlSugarInstance分部映射文件SqlSugarTablesMapping.cs
 * 文件作者：新生帝
-* 编写日期：2016-02-14 15:33:00
+* 编写日期：2016-02-14 21:55:57
 * 版权所有：中山赢友网络科技有限公司
 * 企业官网：http://www.winu.net
 * 开源协议：GPL v2 License
