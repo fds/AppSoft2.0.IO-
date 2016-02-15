34b52b22b612bb51287aee3db302231013112310
中山赢友网络科技有限公司 <winu@winu.net>
2016-02-16 00:51:52 +0800

    1、优化模板生成器，取消设置解决方案目录，通过T4模板动态获取解决方案目录

--- App.Services/UserServices.cs
+++ App.Services/UserServices.cs
@@ -6,7 +6,7 @@ using App.IRepository;
 /*!
 * 文件名称：UserServices服务类
 * 文件作者：新生帝
-* 编写日期：2016-02-15 10:58:15
+* 编写日期：2016-02-16 00:50:14
 * 版权所有：中山赢友网络科技有限公司
 * 企业官网：http://www.winu.net
 * 开源协议：GPL v2 License
